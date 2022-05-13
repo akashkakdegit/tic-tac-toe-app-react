9cb942613a98968350646200a157e20d20cb84ed
akash <akashkakde1996@gmail.com>
2022-05-13 09:00:11 +0530

    initial commit

---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Square(props) {
+    return (
+        <div>
+            <button className="btn" onClick={() => props.onClick()}>{props.value}</button>
+        </div>
+    )
+}
+export default Square;
